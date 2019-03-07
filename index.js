2168d28c1b8b8b5952a3bf0cdc4293419f6b07be
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-03-07 18:32:43 -0300

    Add more rules

--- index.js
+++ index.js
@@ -2,35 +2,73 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
-    'plugin:prettier/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier',
     'prettier/@typescript-eslint',
     'prettier/react',
   ],
-  plugins: ['react-hooks'],
+  plugins: ['@typescript-eslint', 'react-hooks', 'prettier'],
+
   parser: '@typescript-eslint/parser',
-  env: {
-    es6: true,
-    browser: true,
-    node: true,
-  },
-  rules: {
-    'react/jsx-filename-extension': [
-      1,
-      { extensions: ['.js', '.jsx', '.tsx'] },
-    ],
-    'react/prop-types': 'off',
-    'no-underscore-dangle': [1, { allow: ['_id', '__NAME__'] }],
-    semi: ['error', 'never'],
-    quotes: ['error', 'single'],
-    'comma-dangle': ['error', 'always-multiline'],
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/interface-name-prefix': 'off',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
   settings: {
     react: {
       version: 'detect',
     },
+  },
+
+  rules: {
+    // pretier
+    'prettier/prettier': 'error',
+
+    // override eslint-config-prettier options
+    curly: ['error', 'all'],
+    quotes: [
+      'error',
+      'single',
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: false,
+      },
+    ],
+
+    // eslint base rules
+    'no-shadow-restricted-names': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        vars: 'local',
+        args: 'all',
+        argsIgnorePattern: '^_',
+        ignoreRestSiblings: true,
+        caughtErrors: 'all',
+      },
+    ],
+    'one-var': ['error', 'never'],
+    'no-var': 'error',
+
+    'no-useless-computed-key': 'error',
+    radix: 'error',
+    yoda: 'error',
+
+    // typescript
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+
+    // react-specific rules
+    'react/no-this-in-sfc': 'error',
+    'react/no-typos': 'error',
+    'react/prop-types': ['error', { skipUndeclared: true }],
+
+    'react/jsx-boolean-value': 'error',
+    'react/jsx-fragments': ['error', 'syntax'],
+
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
 }
