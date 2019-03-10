87f4e4ce30665da1b84ea75ed89d695c8a7a1eba
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-03-10 19:46:22 -0300

    More rules

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   extends: [
+    'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
     'plugin:jsx-a11y/recommended',
@@ -38,7 +39,25 @@ module.exports = {
     ],
 
     // eslint base rules
+
+    // possible errors https://eslint.org/docs/rules/#possible-errors
+    'no-async-promise-executor': 'error',
+    'no-await-in-loop': 'error',
+    'no-template-curly-in-string': 'error',
+    'require-atomic-updates': 'error',
+    'no-extra-parens': 'error',
+
+    // best practices https://eslint.org/docs/rules/#best-practices
+    'no-else-return': 'error',
+    'no-extra-bind': 'error',
+    'no-new-func': 'error',
+    'no-new-wrappers': 'error',
+    radix: 'error',
+    yoda: 'error',
+
+    // variables https://eslint.org/docs/rules/#variables
     'no-shadow-restricted-names': 'error',
+    'no-undefined': 'error',
     '@typescript-eslint/no-unused-vars': [
       'error',
       {
@@ -49,12 +68,13 @@ module.exports = {
         caughtErrors: 'all',
       },
     ],
-    'one-var': ['error', 'never'],
-    'no-var': 'error',
 
+    // stylistic issues https://eslint.org/docs/rules/#stylistic-issues
+    'one-var': ['error', 'never'],
+
+    // es6 https://eslint.org/docs/rules/#ecmascript-6
+    'no-var': 'error',
     'no-useless-computed-key': 'error',
-    radix: 'error',
-    yoda: 'error',
 
     // typescript
     '@typescript-eslint/explicit-function-return-type': 'off',
