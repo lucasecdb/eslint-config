c9f27e8554c4c96985cc8c5f9e92027c585a92da
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-06-17 18:32:27 -0300

    update typescript-specific rules

--- rules/typescript.js
+++ rules/typescript.js
@@ -43,8 +43,8 @@ module.exports = {
           },
         ],
 
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/naming-convention': 'warn',
       },
     },
     {
